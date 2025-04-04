4a0b157980a62f525be545d517a0c7b43b0d9932
parthi-n <parthiban.n@live.com>
2025-04-04 14:28:57 +0800

    fixed bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,23 +6,23 @@ export async function middleware(req: NextRequest) {
 	const cookie = req.cookies.get("token");
 	console.log('Token cookie:', cookie);
 
-	if (!cookie || !cookie.value) {
-		console.log("no cookie");
-		return NextResponse.redirect(new URL("/test", req.url));
-	}
+	// if (!cookie || !cookie.value) {
+	// 	console.log("no cookie");
+	// 	return NextResponse.redirect(new URL("/test", req.url));
+	// }
 
-	try {
-		const isValid = await verifyToken();
-		console.log("isValid", isValid);
-		// if (cookie && isValid) {
-		// 	return NextResponse.next();
-		// } else {
-		// 	return NextResponse.redirect(new URL("/", req.url));
-		// }
-	} catch (error) {
-		console.log("Invalid or expired token", error);
-		return NextResponse.redirect(new URL("/", req.url));
-	}
+	// try {
+	// 	const isValid = await verifyToken();
+	// 	console.log("isValid", isValid);
+	// 	// if (cookie && isValid) {
+	// 	// 	return NextResponse.next();
+	// 	// } else {
+	// 	// 	return NextResponse.redirect(new URL("/", req.url));
+	// 	// }
+	// } catch (error) {
+	// 	console.log("Invalid or expired token", error);
+	// 	return NextResponse.redirect(new URL("/", req.url));
+	// }
 }
 
 export const config = {
