6a94c034101339bb739935369f3e14b7425bea4b
parthi-n <parthiban.n@live.com>
2025-04-04 16:08:25 +0800

    fixed bug

--- src/lib/getCookie.js
+++ src/lib/getCookie.js
@@ -2,9 +2,8 @@ import { cookies } from "next/headers";
 
 const getToken = async () => {
 	const cookieStore = await cookies();
-	const storageToken = localStorage.getItem("token");
 
-	const token = `token=${cookieStore.get("token")?.value}` || `token=${storageToken}`;
+	const token = `token=${cookieStore.get("token")?.value}`;
 	return token;
 };
 
