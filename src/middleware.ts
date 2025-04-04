3a0717241c421618ebf49301dfccdc3a9832b976
parthi-n <parthiban.n@live.com>
2025-04-04 13:09:57 +0800

    fixed bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,23 +3,23 @@ import type { NextRequest } from "next/server";
 import { verifyToken } from "./app/api/(auth)/verifyToken";
 
 export async function middleware(req: NextRequest) {
-	// const cookie = req.cookies.get("token");
+	const cookie = req.cookies.get("token");
 
-	// if (!cookie || !cookie.value) {
-	// 	return NextResponse.redirect(new URL("/", req.url));
-	// }
+	if (!cookie || !cookie.value) {
+		return NextResponse.redirect(new URL("/", req.url));
+	}
 
-	// try {
-	// 	const isValid = await verifyToken();
-	// 	if (cookie && isValid) {
-	// 		return NextResponse.next();
-	// 	} else {
-	// 		return NextResponse.redirect(new URL("/", req.url));
-	// 	}
-	// } catch (error) {
-	// 	console.log("Invalid or expired token", error);
-	// 	return NextResponse.redirect(new URL("/", req.url));
-	// }
+	try {
+		const isValid = await verifyToken();
+		if (cookie && isValid) {
+			return NextResponse.next();
+		} else {
+			return NextResponse.redirect(new URL("/", req.url));
+		}
+	} catch (error) {
+		console.log("Invalid or expired token", error);
+		return NextResponse.redirect(new URL("/", req.url));
+	}
 }
 
 export const config = {
