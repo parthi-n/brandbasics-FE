61fb84dafac30408f1159162d1f2c71ec9215cd7
parthi-n <parthiban.n@live.com>
2025-04-04 12:59:30 +0800

    fixed bug

--- src/lib/getCookie.js
+++ src/lib/getCookie.js
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 const getToken = async () => {
 	const cookieStore = await cookies();
 	const token = `token=${cookieStore.get("token")?.value}`;
+	console.log("Token:", token);
 	return token;
 };
 
