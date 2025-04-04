99e45f59e3df9c61fd3344defc62840bfa47add1
parthi-n <parthiban.n@live.com>
2025-04-04 13:28:33 +0800

    fixed bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(req: NextRequest) {
 
 	if (!cookie || !cookie.value) {
 		console.log("no cookie");
-		return NextResponse.redirect(new URL("/", req.url));
+		return NextResponse.redirect(new URL("/hello", req.url));
 	}
 
 	try {
